0d7e7087bcbd052c402f9d0f48441dd3a432afde
Ritesh Kumar shukla <riteshshukla2381@gmail.com>
2022-01-02 21:01:14 +0530

    Print

--- src/Components/Background/background.js
+++ src/Components/Background/background.js
@@ -1,5 +1,5 @@
 import './background.css'
-import image from './Layer 2.png'
+
 export default function Background(props,option){
     const style={
         "background-image": `url('https://i.pinimg.com/originals/ae/a4/d6/aea4d61eefe73e341660ec17118ec0fe.jpg')`
